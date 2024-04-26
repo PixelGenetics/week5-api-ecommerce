408fb444bcd63d3f0881ee1b1f41d411d245cbbe
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-04-25 18:21:17 -0600

    Creating user tests

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -48,3 +48,50 @@ test("POST => BASE_URL, should return statusCode 201, and res.body.body.firstNam
         expect(res.body).toBeDefined()
         expect(res.body.firstName).toBe(user.firstName)
 })
+
+test("PUT => 'BASE_URL/:id', should return status code 200, res.body.firstName === userUpdate.lastName", async() => {
+    const userUpdate = {
+        lastName: "Andre"
+    }
+
+    const res = await request(app)
+        .put(`${BASE_URL}/${userId}`)
+        .send(userUpdate)
+        .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.lastName).toBe(userUpdate.lastName)
+})
+
+test("POST => 'BASE_URL/login', should return statusCode(200), and res.body.email === user.email and res.body.token to be defined", async() =>{
+    const user = {
+        email:"AG@gmail.com",
+        password:"amsdksm",
+    }
+    const res = await request(app)
+        .post(`${BASE_URL}/login`)
+        .send(user)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.user.email).toBe(user.email)
+    expect(res.body.token).toBeDefined()
+})
+
+test("Delete => 'BASE_URL/:id', shoudl return statusCode 204", async() => {
+    const res = request(app)
+    .delete(`${BASE_URL}/${userId}`)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect((await res).statusCode).toBe(204)
+})
+
+// const resBody = {
+//     user:{
+//         firstName: "",
+//         lastName: "",
+//         email: ""
+//     },
+//     token: ""
+// }
